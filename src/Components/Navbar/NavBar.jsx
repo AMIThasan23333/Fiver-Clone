de5316b3785de8b4bb7cad6f05cf3df28efeea63
My Name <cpgsom819@gmail.com>
2023-07-17 23:47:30 +0800

    navbar css

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./NavBar.scss";
 
 const NavBar = () => {
   return (
@@ -9,8 +10,25 @@ const NavBar = () => {
             <span className='text'>Fiverr</span>
             <span className='dot'>.</span>
         </div>
-          <div className="links"></div>
+          <div className="links">
+          <span>Liverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign In</span>
+          <span>Become a Seller </span>
+          <button>Join</button>
+
+          </div>
+        
        </div>
+       <hr/>
+
+
+     <div className="menu">
+        <span>Test</span>
+        <span>Test2</span>
+     </div>
+
     </div>
   )
 }
