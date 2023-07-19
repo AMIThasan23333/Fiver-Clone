de9ad5114483deb759424fddeb87ee7ac71a04a6
My Name <cpgsom819@gmail.com>
2023-07-20 03:04:40 +0800

    trustedby section done

--- src/Components/TrustedBy/TrustedBy.jsx
+++ src/Components/TrustedBy/TrustedBy.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 import './TrustedBy.scss';
 const TrustedBy = () => {
   return (
-    <div>TrustedBy</div>
+  
+    <div className="trustedBy">
+
+     <div className="container">
+     <span>Trusted By :</span>
+
+    <img src='/src/img/facebook.png'></img>
+    <img src='/src/img/instagram.png'></img>
+    <img src='/src/img/pinterest.png'></img>
+    <img src='/src/img/linkedin.png'></img>
+    <img src='/src/img/twitter.png'></img>
+
+     </div>
+
+    </div>
   )
 }
 
