e6985930d861e4f114934beb6423aae69c497ec1
My Name <cpgsom819@gmail.com>
2023-07-28 01:32:03 +0800

    adding gig

--- src/Components/CatCard/CatCard.jsx
+++ src/Components/CatCard/CatCard.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './CatCard.scss'
+
+
 const CatCard = ({item}) => {
-
-  console.log(item.desc);
-
-
+  
   return (
-
-
-    <Link to='/gig/123'>
+    <Link to="/gigs">
 
     <div className="catCard">
-
-
        <span className='desc'>{item.desc}</span>
       <span className='desc'>{item.title}</span>
       <img src={item.img} alt="" sizes="" srcset="" />
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -73,11 +73,11 @@ function Footer() {
           </div>
           <div className="right">
             <div className="social">
-              <img src="../../img/twitter.png" alt="" />
-              <img src="../../img/facebook.png" alt="" />
-              <img src="../../img/instagram.png" alt="" />
-              <img src="../../img/pinterest.png" alt="" />
-              <img src="../../img/linkedin.png" alt="" />
+              <img src="/img/twitter.png" alt="" />
+              <img src="/img/facebook.png" alt="" />
+              <img src="/img/linkedin.png" alt="" />
+              <img src="/img/pinterest.png" alt="" />
+              <img src="/img/instagram.png" alt="" />
             </div>
             <div className="link">
               <img src="/img/language.png" alt="" />
